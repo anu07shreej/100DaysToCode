030961ea7b6acca4c9e0157be64244b4433eb339
ajumade <anu0703shree@gmail.com>
2024-10-24 17:57:40 -0700

    Day1 files

--- Python Bootcamp/Day 1/Day1.py
+++ Python Bootcamp/Day 1/Day1.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+print("Hello, World!\nHello,World!!\nHello,World!!")
+
+#Comments.. more comments
+nme= input("What is your name?")
+print(nme + " That's a wonderful name!")
+
+#Band Name Generator
+city_name = input("In which city you grew up?")
+pet_name = input("What was your pet?")
+print("Your band name could be: "+city_name + " " + pet_name )
