fca87d0da2e0b2851d8397045cb8818dd91a4186
ajumade <anu0703shree@gmail.com>
2024-10-26 23:19:22 -0700

    Day3 files

---
+++ Python Bootcamp/Day 3/Day 3.py
@@ -0,0 +1,5 @@
+print("Welcome to Pizza Deliveries")
+
+size = input("What size pizza do you want? S, M or L")
+pepperoni = input("Want pepperoni on pizza? Y or N")
+extra_cheese = input("Want extra cheese?  Y or N")
