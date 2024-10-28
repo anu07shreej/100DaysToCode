d533939d5779a83c486b597888cd916a432bf99c
ajumade <anu0703shree@gmail.com>
2024-10-27 17:44:34 -0700

    Day5 files

---
+++ Python Bootcamp/Day 5/Day 5.py
@@ -0,0 +1,22 @@
+#Highest score
+# student_scores = [1,2,3,4,5,6,7]
+#
+# max_score = 0
+# for score in student_scores:
+#     if score > max_score:
+#         max_score = score
+# print("max score is: " + str(max_score))
+#----------FIZZBUZZ--------
+# for i in range (1,101):
+#     if i % 15 == 0:
+#         print("FizzBuzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(i)
+
+#---------------------PASSWORD GENERATOR-----------------
+print("Welcome to the PyPassword Generator")
+
