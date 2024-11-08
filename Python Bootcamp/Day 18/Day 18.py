75af752558945fd282448976132d20d314131330
ajumade <anu0703shree@gmail.com>
2024-11-07 22:39:33 -0800

    Day 18 Files

---
+++ Python Bootcamp/Day 18/Day 18.py
@@ -0,0 +1,32 @@
+from turtle import Turtle, Screen
+#import heroes
+
+
+tim = Turtle()
+screen = Screen()
+
+tim.shape('arrow')
+#-----DRAW a SQUARE
+# for i in range(20):
+#     tim.pendown()
+#     tim.forward(5)
+#     tim.penup()
+#     tim.forward(5)
+
+def draw_shape(sides):
+    for i in range(sides):
+        tim.forward(10)
+        tim.h
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
