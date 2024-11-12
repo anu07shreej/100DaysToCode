0c854b9b067d132191aaded185e039225f6383d8
ajumade <anu0703shree@gmail.com>
2024-11-12 10:09:25 -0800

    Day 20 Snake Game

--- Python Bootcamp/Day 20/snake_game.py
+++ Python Bootcamp/Day 20/snake_game.py
@@ -1,10 +1,12 @@
 from turtle import Turtle, Screen
 import random
+import time
 
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 start_positions = [(0,0),(-20,0),(-40,0)]
 segment_list = []
@@ -18,13 +20,14 @@ for start in start_positions:
 
 game_is_on = True
 while game_is_on:
-    for seg in segment_list:
-        seg.forward(20)
-
-
-
-
-
+    screen.update()
+    time.sleep(0.2)
+    for seg_num in range(len(segment_list)-1, 0, -1):
+        new_x = segment_list[seg_num - 1].xcor()
+        new_y = segment_list[seg_num - 1].ycor()
+        segment_list[seg_num].goto(new_x,new_y)
+    segment_list[0].forward(20)
+    segment_list[0].left(90)
 
 
 
